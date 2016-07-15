e9b3a67670094c7a3c690e155e7d9a2ab66a0cbc
Hannah_Brannan <hannahbrannan@gmail.com>
2016-07-14 20:00:30 -0700

    Start em shemas

---
+++ server/schemas.js
@@ -0,0 +1,28 @@
+var db = require('./server.js')
+var Sequalize = require('sequalize');
+
+
+var sequelize = new Sequelize(db.dbUrl, {
+	dialect:'postgres'
+	// protocol:'postgres'
+});
+
+
+////////////POSTGRES Tables///////////////////
+//////////////sequelize???/////////////
+
+
+var User = sequelize.define('user', {
+	firstName: {
+		type: Sequelize.STRING,
+		field: 'firstName'
+	}, 
+	lastName: {
+		type: Sequelize.STRING,
+		field: 'lastName'
+	},
+	phoneNumber: {
+		type: Sequelize.INTEGER,
+		field: 'lastName'
+	},
+})
--- server/server.js
+++ server/server.js
@@ -5,24 +5,35 @@ var pg = require('pg');
 
 ////SET VARIABLES
 var port = 8080;
-var dbUrl = 'https://git.heroku.com/heroku-postgres-9da13075.git';
-
+//postgres:// ??
+var dbUrl = process.env.DATABASE_URL || 'postgres://git.heroku.com/heroku-postgres-9da13075.git';
+process.env.dbUrl = dbUrl;
 
 ////CONNECT TO SERVER
 var app = express();
 app.listen(port, function () {
-	console.log('HIPS HIPS HIPS' + port);
+	console.log('HIPS HIPS HIPS ' + port);
 });
 
 
 ////CONNECT TO DB
 // pg.defaults.ssl = true;
 
-pg.connect(process.env.dbUrl, function (err, client, done) {
-	console.log('done', done);
-	if (err) {
-		return console.error(err);
-	} else {
-		console.log('client', client);
-	}
+// var db = pg.connect(process.env.dbUrl, function (err, client, done) {
+// 	console.log('done', done);
+// 	if (err) {
+// 		return console.error(err);
+// 	} else {
+// 		console.log('client', client);
+// 	}
+// });
+
+var sequelize = new Sequelize(dbUrl, {
+	dialect:'postgres'
+	// protocol:'postgres'
 });
+
+module.exports = {
+	dbUrl: dbUrl
+};
+
