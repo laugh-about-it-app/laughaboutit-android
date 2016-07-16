49818617b9f84ca8f99a676371652142c3701dcb
Christopher Pruijsen <christopher.pruijsen@gmail.com>
2016-07-15 20:10:30 -0700

    initial - facebook iOS RN working, start on Digits

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,34 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  Navigator,
   Text,
-  View
-} from 'react-native';
+  View,
+  Image,
+} from 'react-native'
+import Main from './ios/App/Components/Main';
 
-class laughaboutit extends Component {
-  render() {
+ class test2 extends Component {
+  
+   renderScene(route, navigator) {
+    
+    if(route.name === 'Main') {
+      return <Main title={'Main'} navigator={navigator} {...route.passProps}/>
+    }
+   // other routes.
+   }
+   
+   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Navigator
+        style={{flex: 1}}
+        initialRoute={{name: 'Main'}}
+        renderScene={ this.renderScene } 
+      />       
     );
   }
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
-AppRegistry.registerComponent('laughaboutit', () => laughaboutit);
+ AppRegistry.registerComponent('test2', () => test2);
